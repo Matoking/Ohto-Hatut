56913b4a8148fcf794717b3ec9b4cbf4b13d75f6
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-04-12 10:58:54 +0300

    references have a list of their fields

--- src/main/java/ohtuhatut/domain/BookReference.java
+++ src/main/java/ohtuhatut/domain/BookReference.java
@@ -1,5 +1,6 @@
 package ohtuhatut.domain;
 
+import java.util.ArrayList;
 import ohtuhatut.domain.Reference;
 
 import javax.persistence.DiscriminatorValue;
@@ -18,4 +19,24 @@ import javax.persistence.Table;
 @DiscriminatorValue("book")
 @Entity
 public class BookReference extends Reference {
+    
+    public BookReference() {
+        fields = new ArrayList<>();
+        fields.add("author");
+        fields.add("title");
+        fields.add("publisher");
+        fields.add("year");
+    }
+    
+    public String getField(String field) {
+        if (field.equals("author")) {
+            return author;
+        } else if (field.equals("title")) {
+            return title;
+        } else if (field.equals("publisher")) {
+            return publisher;
+        }
+        
+        return "" + year;
+    }
 }
