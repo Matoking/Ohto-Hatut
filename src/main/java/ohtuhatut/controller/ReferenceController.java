b530867e49681c9e6c642138d70736cebaae096c
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-04-12 12:02:18 +0300

    can add article references now

--- src/main/java/ohtuhatut/controller/ReferenceController.java
+++ src/main/java/ohtuhatut/controller/ReferenceController.java
@@ -1,8 +1,10 @@
 
 package ohtuhatut.controller;
 
+import ohtuhatut.domain.ArticleReference;
 import ohtuhatut.domain.BookReference;
 import ohtuhatut.domain.Reference;
+import ohtuhatut.repository.ArticleReferenceRepository;
 import ohtuhatut.repository.BookReferenceRepository;
 import ohtuhatut.repository.ReferenceListRepository;
 import ohtuhatut.repository.ReferenceRepository;
@@ -30,32 +32,54 @@ public class ReferenceController {
     private BookReferenceRepository bookReferenceRepository;
     @Autowired
     private ReferenceListRepository referenceListRepository;
+    @Autowired
+    private ArticleReferenceRepository articleReferenceRepository;
 
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String newReference(Model model){        
+        return "reference_choose";
+    }
+    
+    // works for all kind of references
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getReference(Model model, @PathVariable Long id){     
         model.addAttribute("reference", referenceRepository.findOne(id));      
         return "reference";
     }
     
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
-    public String newReference(Model model){        
-        return "reference_choose";
-    }
-    
+    // -------------- book references
     @RequestMapping(value = "/bookreferences/new", method = RequestMethod.GET)
     public String newBookReference(Model model){;
         model.addAttribute("reference", new BookReference());
-        
+        model.addAttribute("referenceType", "bookreferences");
         return "reference_new";
     }
     
     @RequestMapping(value = "/bookreferences/new", method = RequestMethod.POST)
     public String newBookReferenceCreate(@ModelAttribute BookReference reference, RedirectAttributes attr) {
-        bookReferenceRepository.save(reference);
+        bookReferenceRepository.save(reference);       
         
         attr.addAttribute("id", reference.getId());
         return "redirect:/references/{id}";
     }
+    // <-- book references
+    
+    // -------------- article references
+    @RequestMapping(value = "/articlereferences/new", method = RequestMethod.GET)
+    public String newArticleReference(Model model) {
+        model.addAttribute("reference", new ArticleReference());
+        model.addAttribute("referenceType", "articlereferences");
+        return "reference_new";
+    }
+    
+    @RequestMapping(value = "/articlereferences/new", method = RequestMethod.POST)
+    public String newArticleReferenceCreate(@ModelAttribute ArticleReference reference, RedirectAttributes attr) {
+        articleReferenceRepository.save(reference);
+       
+        attr.addAttribute("id", reference.getId());
+        return "redirect:/references/{id}";
+    }
+    // <-- article references
     
 }
 
