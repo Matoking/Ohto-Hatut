71226a6395d9ad11a11be538e5b34333e248783f
iilumme <iilumme@cs.helsinki.fi>
2016-04-13 07:24:39 +0300

    Refactor of controllers

--- src/main/java/ohtuhatut/controller/ReferenceListController.java
+++ src/main/java/ohtuhatut/controller/ReferenceListController.java
@@ -1,6 +1,7 @@
 package ohtuhatut.controller;
 
 import ohtuhatut.service.ReferenceListService;
+import ohtuhatut.service.ReferenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ohtuhatut.domain.Reference;
 import ohtuhatut.domain.ReferenceList;
-import ohtuhatut.repository.ReferenceListRepository;
 import ohtuhatut.repository.ReferenceRepository;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,20 +26,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/referencelists")
 public class ReferenceListController {
-
-    @Autowired
-    private ReferenceListRepository referenceListRepository;
     
     @Autowired
     private ReferenceListService referenceListService;
-    
+
     @Autowired
-    private ReferenceRepository referenceRepository;
+    private ReferenceService referenceService;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getReferenceList(Model model, @PathVariable Long id){
         model.addAttribute("referenceList", referenceListService.getReferenceList(id));
-        model.addAttribute("references", referenceRepository.findAll());
+        model.addAttribute("references", referenceService.getAllReferences());
 
         return "referenceList";
     }
@@ -53,7 +50,7 @@ public class ReferenceListController {
     
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public String newReferenceListCreate(@ModelAttribute ReferenceList referenceList, RedirectAttributes attr){
-        referenceListRepository.save(referenceList);
+        referenceListService.save(referenceList);
         
         attr.addAttribute("id", referenceList.getId().toString());
         return "redirect:/referencelists/{id}";
@@ -64,11 +61,11 @@ public class ReferenceListController {
             @RequestParam(value="referenceId") Long referenceId,
             RedirectAttributes redirectAttrs) {
         
-        ReferenceList list = referenceListRepository.findOne(id);
-        Reference reference = referenceRepository.findOne(referenceId);
+        ReferenceList list = referenceListService.getReferenceList(id);
+        Reference reference = referenceService.getReference(referenceId);
         
         list.getReferences().add(reference);
-        referenceListRepository.save(list);        
+        referenceListService.save(list);
         
         redirectAttrs.addAttribute("id", id);
         
--- src/main/java/ohtuhatut/service/ReferenceListService.java
+++ src/main/java/ohtuhatut/service/ReferenceListService.java
@@ -1,13 +1,18 @@
 package ohtuhatut.service;
 
+import ohtuhatut.domain.Reference;
 import ohtuhatut.domain.ReferenceList;
 import ohtuhatut.repository.ReferenceListRepository;
+import ohtuhatut.repository.ReferenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 /**
- * Created by iilumme.
+ * @author iilumme
  */
 
 @Service
@@ -19,4 +24,9 @@ public class ReferenceListService {
     public ReferenceList getReferenceList(Long id) {
         return referenceListRepository.findOne(id);
     }
+
+    public void save(ReferenceList referenceList) {
+        referenceListRepository.save(referenceList);
+    }
+
 }
