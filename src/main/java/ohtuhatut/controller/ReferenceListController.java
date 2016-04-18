cd03ebe3b8ac1385419386dce4cfc6c5468f885c
iilumme <iilumme@cs.helsinki.fi>
2016-04-18 06:36:27 +0300

    refactoring

--- src/main/java/ohtuhatut/controller/ReferenceListController.java
+++ src/main/java/ohtuhatut/controller/ReferenceListController.java
@@ -9,10 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import ohtuhatut.domain.Reference;
 import ohtuhatut.domain.ReferenceList;
-import ohtuhatut.repository.ReferenceRepository;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
