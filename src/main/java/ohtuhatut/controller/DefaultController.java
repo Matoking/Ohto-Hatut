fa937a2d62a34e2d200e7f7054b4aad0080ed971
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-04-19 19:01:59 +0300

    removed left over unnecessary code

--- src/main/java/ohtuhatut/controller/DefaultController.java
+++ src/main/java/ohtuhatut/controller/DefaultController.java
@@ -1,8 +1,6 @@
 package ohtuhatut.controller;
 
-import ohtuhatut.domain.BookReference;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,22 +13,5 @@ public class DefaultController {
     @RequestMapping("*")
     public String index() {
         return "index";
-    }
-    
-    /**
-     * Create a test book reference and try displaying it
-     * @return 
-     */
-    @RequestMapping("/testReference")
-    public String testReference(Model model) {
-        BookReference testReference = new BookReference();
-        
-        testReference.setTitle("Testikirja");
-        testReference.setPublisher("Testikirjailijat");
-        testReference.setYear(2010);
-        
-        model.addAttribute("testReference", testReference);
-        
-        return "testReference";
     }
 }
