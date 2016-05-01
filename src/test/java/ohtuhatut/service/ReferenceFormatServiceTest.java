41d7543d01a4bd8d80c97a61dd5ae7edc835710f
Sofia <sofia.kansikas@gmail.com>
2016-05-01 16:44:15 +0300

    added tests for umlaut O and swedish A

--- src/test/java/ohtuhatut/service/ReferenceFormatServiceTest.java
+++ src/test/java/ohtuhatut/service/ReferenceFormatServiceTest.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
 public class ReferenceFormatServiceTest {
@@ -34,16 +33,42 @@ public class ReferenceFormatServiceTest {
     }
 
     @Test
-    public void specialCharactersAreFormattedProperly() {
+    public void umlautAIsFormatterProperly() {
 
         List<Reference> references = new ArrayList<>();
         Reference ref = new ManualReference();
-        
+
         ref.setKey("key1");
-        ref.setTitle("tätä");
+        ref.setTitle("tÄtä");
         references.add(ref);
-       
-        assertTrue(referenceFormatService.formatReferencesToBibTeX(references).contains("t{\\\"a}t{\\\"a}"));
+
+        assertTrue(referenceFormatService.formatReferencesToBibTeX(references).contains("t{\\\"A}t{\\\"a}"));
+    }
+
+    @Test
+    public void umlautOIsFormatterProperly() {
+
+        List<Reference> references = new ArrayList<>();
+        Reference ref = new ManualReference();
+
+        ref.setKey("key1");
+        ref.setTitle("tÖtö");
+        references.add(ref);
+
+        assertTrue(referenceFormatService.formatReferencesToBibTeX(references).contains("t{\\\"O}t{\\\"o}"));
+    }
+
+    @Test
+    public void swedishAIsFormatterProperly() {
+
+        List<Reference> references = new ArrayList<>();
+        Reference ref = new ManualReference();
+
+        ref.setKey("key1");
+        ref.setTitle("tÅtå");
+        references.add(ref);
+
+        assertTrue(referenceFormatService.formatReferencesToBibTeX(references).contains("t{\\AA}t{\\aa}"));
     }
 
 }
