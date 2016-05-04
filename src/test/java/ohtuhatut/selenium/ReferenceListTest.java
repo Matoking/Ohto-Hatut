64b291f5626e4c68171bac48e25eae6207c73172
iilumme <iilumme@cs.helsinki.fi>
2016-05-04 11:49:54 +0300

    cooler reference_show maybe

--- src/test/java/ohtuhatut/selenium/ReferenceListTest.java
+++ src/test/java/ohtuhatut/selenium/ReferenceListTest.java
@@ -15,6 +15,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.beans.factory.annotation.Autowired;
